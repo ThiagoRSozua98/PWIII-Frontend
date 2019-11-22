c07b6709d9018468ee26eef03a42e6fc0e879712
Juan Thiago <thiago.reiissouza@gmail.com>
2019-11-22 20:13:28 -0300

    ex.23-1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListaClienteComponent } from './lista-cliente/lista-cliente.component';
 import { ListaUsuarioComponent } from './lista-usuario/lista-usuario.component';
 import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaClienteComponent,
     ListaUsuarioComponent,
     NovoUsuarioComponent
   ],
