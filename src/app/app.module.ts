a973cd3748ed4b0ebeb8823ade352ddc1ba1a253
Juan Thiago <thiago.reiissouza@gmail.com>
2019-11-22 20:02:54 -0300

    ex.22-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaClienteComponent } from './lista-cliente/lista-cliente.component';
+import { ListaUsuarioComponent } from './lista-usuario/lista-usuario.component';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaClienteComponent,
+    ListaUsuarioComponent,
+    NovoUsuarioComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/novo-usuario/novo-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'has-novo-usuario',
+  templateUrl: './novo-usuario.component.html',
+  styleUrls: ['./novo-usuario.component.css']
+})
+export class NovoUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
