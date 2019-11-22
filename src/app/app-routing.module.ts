addf05db1965001fd97bd4701722deff2adf64d9
Juan Thiago <thiago.reiissouza@gmail.com>
2019-11-22 20:10:40 -0300

    ex.23-1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaUsuarioComponent } from './lista-usuario/lista-usuario.component';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'listagem', component: ListaUsuarioComponent },
+  { path: 'novo', component: NovoUsuarioComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
