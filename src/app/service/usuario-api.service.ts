f7d9e9703ca23110b34cb600425876c3c62be47c
Juan Thiago <thiago.reiissouza@gmail.com>
2019-11-22 22:40:03 -0300

    ex.27-1.1

--- src/app/service/usuario-api.service.ts
+++ src/app/service/usuario-api.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { throwError, Observable } from 'rxjs';
 import { Usuario } from '../usuario';
-
+import { retry, catchError } from 'rxjs/operator';
 
 @Injectable({
   providedIn: 'root'
