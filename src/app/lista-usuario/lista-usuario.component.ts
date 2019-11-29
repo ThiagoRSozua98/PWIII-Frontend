955c4f59b29b41ade5d688be67a56df32e0cc4e9
Juan/Thiago <thiago.reiissouza@gmail.com>
2019-11-29 20:08:53 -0300

    ex.28-1

--- src/app/lista-usuario/lista-usuario.component.ts
+++ src/app/lista-usuario/lista-usuario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioAPIService } from '../service/usuario-api.service';
+import { Usuario } from '../usuario';
 
 @Component({
   selector: 'has-lista-usuario',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-usuario.component.css']
 })
 export class ListaUsuarioComponent implements OnInit {
+  usuarios: Usuario[];
 
-  constructor() { }
+  constructor(private service: UsuarioAPIService) { }
 
   ngOnInit() {
+    this.service
+        .getUsuario()
+        .subscribe((data: Usuario[]) => this.usuarios = data,
+                    error => console.log(error));
   }
 
 }
